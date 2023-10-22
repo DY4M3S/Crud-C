1b6834f4fb366a2638928959aff772b1304e5fe2
DY4M3S <dyamepdu@gmail.com>
2023-10-22 16:38:46 -0300

    Base do projeto

---
+++ Crud.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+     printf("Seja bem-vindos a loja All T-Shirt!!\n\n Menu principal:\n\n 1-Adiconar um novo registro\n 2-Listar todos os registros\n 0-Sair do programa\n");
+     
+
+    
+
+     return 0;
+}
