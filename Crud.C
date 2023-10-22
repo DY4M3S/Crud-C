a5dbecd1eab13cea06c762f84f55d2b0875c5081
DY4M3S <dyamespdu@gmail.com>
2023-10-22 18:31:47 -0300

    Criação da função menu

--- Crud.C
+++ Crud.C
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <conio.h>
+
+int selecione(int alternativa){
+if  (alternativa==1){
+     printf("Adicionar Registros");
+     
+}else if(alternativa==2){
+     printf("Listar Registros");
+}else if(alternativa==0){
+     printf("Deseja sair do programa?");  
+}
+
+}
+
+void menu(){
+     printf("Seja bem-vindos a loja All T-Shirt!!\n\n Menu principal:\n\n 1-Adiconar um novo registro\n 2-Listar todos os registros\n 0-Sair do programa\n");
+     int alternativa;
+     scanf( "%d",&alternativa);
+     selecione(alternativa);
+}
+
+
+
 int main()
 {
-     printf("Seja bem-vindos a loja All T-Shirt!!\n\n Menu principal:\n\n 1-Adiconar um novo registro\n 2-Listar todos os registros\n 0-Sair do programa\n");
-     
-
+     menu();
     
-
      return 0;
 }
