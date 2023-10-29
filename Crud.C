3aaa632d9ed7fb371ee1e6913e45e69d741a3ec7
DY4M3S <dyamepdu@gmail.com>
2023-10-29 19:25:42 -0300

    melhorias da funcao menu, criacao da funcao voltar e sair

--- Crud.C
+++ Crud.C
@@ -1,10 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int menu()
+{
+     int alternativa;
+
+     printf("Seja bem-vindos a loja All T-Shirt!!\n\n Menu principal:\n\n 1-Adiconar um novo registro\n 2-Listar todos os registros\n 3-Atualizar as informacoes do registro\n 4-Excluir um registro\n 0-Sair do programa\n\n");
+
+     scanf("%d", &alternativa);
+
+     switch (alternativa)
+     {
+     case 1:
+          printf("Adiconar um novo registro\n\n");
+          break;
+
+     case 2:
+          printf("Listar todos os registros\n\n");
+          break;
+
+     case 3:
+          printf("Atualizar as informacoes do registro\n\n");
+          break;
+
+     case 4:
+          printf("Excluir um registro\n\n");
+          break;
+
+     case 0:
+          exit(0);
+     }
+}
+
+void voltar()
+{
+     menu();
+}
+
+int preco[100];
+char marca[100];
+char modelo[100];
+char tamanho[100];
+char genero[100];
+
 int main()
 {
-     printf("Seja bem-vindos a loja All T-Shirt!!\n\n Menu principal:\n\n 1-Adiconar um novo registro\n 2-Listar todos os registros\n 0-Sair do programa\n");
-     
+     menu();
 
-    
-
-     return 0;
+      return 0;
 }
